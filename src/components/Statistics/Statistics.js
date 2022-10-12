60085d76c2309d310985698204a561f047d6a858
ahmedullah12 <gulamjoy321@gmail.com>
2022-10-12 19:09:37 +0600

    rechart responsive added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -10,8 +10,8 @@ const Statistics = () => {
     return (
        
         <div>
-             
-           <ResponsiveContainer width={"100%"} height={400}>
+             <h1 className='text-2xl lg:text-3xl font-bold mb-20'>Here is a chart of total quizes number</h1>
+           <ResponsiveContainer  width={"75%"} height={200}>
                 <LineChart width={500}  data={data}>
                     <Line type="monotone" dataKey="total" stroke="#000000" strokeWidth={2} />
                     <XAxis dataKey="name" />
