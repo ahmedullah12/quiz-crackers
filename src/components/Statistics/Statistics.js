83f3d10b9bd744a720931c990216113309fc8091
ahmedullah12 <gulamjoy321@gmail.com>
2022-10-12 17:57:33 +0600

    rechart  added in statistic page

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,21 +1,24 @@
 import React from 'react';
-// import { useLoaderData } from 'react-router-dom';
-// import { ResponsiveContainer, LineChart, Line } from 'recharts';
+import { useLoaderData } from 'react-router-dom';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 
 const Statistics = () => {
-    // const dataObj = useLoaderData();
-    // const data = dataObj.data;
-   
+    const dataObj = useLoaderData();
+    const data = dataObj.data;
+   console.log(data)
     
     return (
        
         <div>
              
-           {/* <ResponsiveContainer width="100%" height="100%">
-                <LineChart width={500} height={100} data={data}>
-                <Line type="monotone" dataKey="total" stroke="#82ca9d" strokeWidth={2} />
+           <ResponsiveContainer width={"100%"} height={400}>
+                <LineChart width={500}  data={data}>
+                    <Line type="monotone" dataKey="total" stroke="#000000" strokeWidth={2} />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
                 </LineChart>
-            </ResponsiveContainer> */}
+            </ResponsiveContainer>
         </div>
     );
 };
